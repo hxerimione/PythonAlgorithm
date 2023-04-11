fbef11c306d767b7e7991b2734ae497a35708ae2
hxerimione <purejang98@gmail.com>
2023-04-11 23:50:55 +0900

    update

---
+++ Baekjoon/11052.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+cards = [0] + list(map(int,input().split()))
+dp = [0 for _ in range(N+1)]
+
+for i in range(1,N+1):
+    for k in range(1,i+1):
+        dp[i] = max(dp[i] ,dp[i-k] + cards[k] )
+print(dp[N])
