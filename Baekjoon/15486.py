bf595359b33205aebd3e5f4295c8c6cb34d83e86
hxerimione <purejang98@gmail.com>
2023-04-12 13:52:38 +0900

    update

--- Baekjoon/15486.py
+++ Baekjoon/15486.py
@@ -7,8 +7,9 @@ for _ in range(n):
     a,b = map(int,input().split())
     times.append([a,b])
 dp=[0 for _ in range(n+1)]
-for i in range(n):
-    print(dp)
+for i in range(1,n+1):
+    dp[i] = max(dp[i],dp[i-1])
     if i+times[i][0]-1<=n:
-        dp[i+times[i][0]-1] = dp[i+times[i][0]-2] + max(dp[i+times[i][0]-1],dp[i]+times[i][1])
-print(dp)
+        dp[i+times[i][0]-1] =max(dp[i+times[i][0]-1],dp[i-1]+times[i][1])
+    
+print(dp[-1])
